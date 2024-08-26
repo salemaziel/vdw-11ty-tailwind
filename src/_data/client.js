5738db5b3773fa35a0dc5a2614b5ad9db7466083
Salem Aziel <51006863+salemaziel@users.noreply.github.com>
2024-08-26 15:17:57 -0700

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Cotijas Mexican Grill & Seafood",
+    "email": "support@viadelweb.cloud",
+    "phoneForTel": "619-521-1974",
+    "phoneFormatted": "(619) 521-1974",
+    "address": {
+        "lineOne": "3695 Fairmount Ave",
+        "lineTwo": "",
+        "city": "San Diego",
+        "state": "CA",
+        "zip": "92105",
+        "country": "US",
+        "mapLink": "https://goo.gl/maps/1tjy7V9UZp3z9y3N6"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://cotijas-demo11.netlify.app"
+};
